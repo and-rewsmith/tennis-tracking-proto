c5a8b76c55ce9359a1012eb950919cf674391a72
Andrew <andsmi@microsoft.com>
2021-06-20 19:15:56 -0700

    remove radius detection

--- ball_tracking/ball_tracking.py
+++ ball_tracking/ball_tracking.py
@@ -69,8 +69,8 @@ while True:
 
 	# find contours in the mask and initialize the current
 	# (x, y) center of the ball
-	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-		cv2.CHAIN_APPROX_SIMPLE)[-2]
+	cnts = cv2.findContours(mask.copy(), cv2.RETR_LIST,
+		cv2.CHAIN_APPROX_NONE)[-2]
 	center = None
 
 	print(len(cnts))
@@ -85,17 +85,24 @@ while True:
 		M = cv2.moments(c)
 		print(M)
 
+		desired_radius = .00000000000000000000002
+
 		# break if moment is 0
 		if M["m00"] != 0:
 			center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
 			# only proceed if the radius meets a minimum size
-			if radius > 1:
+			if radius > desired_radius:
 				# draw the circle and centroid on the frame,
 				# then update the list of tracked points
 				cv2.circle(frame, (int(x), int(y)), int(radius),
 					(0, 255, 255), 2)
 				cv2.circle(frame, center, 5, (0, 0, 255), -1)
+		elif radius > desired_radius:
+			print(c)
+			center = (c[0][0][0], c[0][0][1])
+		else:
+			print(radius)
 
 	# update the points queue
 	pts.appendleft(center)
